57df895dc1ac26959455196958945f22b3cca59d
Wagner Rodrigues <wagner.rodrigues.dev@gmail.com>
2024-10-31 17:13:39 -0300

    add docker-compose to external services

--- internal/infrastructure/database/new_db.go
+++ internal/infrastructure/database/new_db.go
@@ -8,7 +8,7 @@ import (
 )
 
 func NewDb() *gorm.DB {
-	dsn := "host=localhost user=emailn_dev password=1234 dbname=emailn_dev port=5432 sslmode=disable"
+	dsn := "host=localhost user=emailn_dev password=1234 dbname=postgres port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
@@ -18,5 +18,3 @@ func NewDb() *gorm.DB {
 	db.AutoMigrate(&campaign.Campaign{}, &campaign.Contact{})
 	return db
 }
-
-//docker run -e POSTGRES_USER=emailn_dev -e POSTGRES_PASSWORD=1234 -p 5432:5432 -d postgres
